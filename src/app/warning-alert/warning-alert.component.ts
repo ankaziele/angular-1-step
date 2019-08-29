d6919ff4353121cf0e7a7dcb55233748c47fcb99
ankaziele <ankaziele@gmail.com>
2019-08-29 13:43:47 +0200

    updating

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
+  template: `
+  <p>this is the new warning</p>
+  `,
   styleUrls: ['./warning-alert.component.css']
 })
 export class WarningAlertComponent implements OnInit {
